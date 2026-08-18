c248dba1ddb25f230f7b7de95817305bd19397bd
ercanlgtv <ercanlgtv@gmail.com>
2026-08-18 20:09:37 +0300

    Add FilmEkseni configuration to filmekseni.js

--- js/filmekseni.js
+++ js/filmekseni.js
@@ -1,1 +1,30 @@
+var rule = {
+    title: 'FilmEkseni',
+    host: 'https://filmekseni.vip',
 
+    homeUrl: '/',
+    url: '/filmler?page=fypage',
+    searchUrl: '/arama?q=**',
+
+    searchable: 2,
+    quickSearch: 0,
+    filterable: 0,
+
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; TV) AppleWebKit/537.36 Chrome/120 Safari/537.36'
+    },
+
+    class_name: 'Filmler&Diziler&Aksiyon&Bilim Kurgu&Gerilim&Komedi&Korku&Suç',
+
+    class_url: 'filmler&diziler&aksiyon&bilim-kurgu&gerilim&komedi&korku&suc',
+
+    推荐: '*',
+
+    一级: '*',
+
+    二级: '*',
+
+    搜索: '*',
+
+    lazy: ''
+};
